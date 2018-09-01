7b74f91c920891791d13db71651dadea2762f6aa
rimadoma <richard.domander@gmail.com>
2018-09-01 18:17:12 +0100

    Add NewsItem unit tests

---
+++ src/test/java/newsscraper/NewsItemTest.java
@@ -0,0 +1,84 @@
+package newsscraper;
+
+import org.junit.Test;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.junit.Assert.assertEquals;
+
+public class NewsItemTest {
+
+    private static final String TEST_TITLE = "Everything is just fine";
+    private static final String TEST_URI = "https://www.bbc.co.uk";
+    private static final String TEST_AUTHOR = "Hans Mustermann";
+
+    @Test
+    public void testGettersSetters() throws URISyntaxException {
+        final NewsItem item = new NewsItem(TEST_TITLE, TEST_URI, TEST_AUTHOR);
+        item.setPoints(1);
+        item.setComments(2);
+        item.setRank(3);
+
+        assertEquals(TEST_TITLE, item.getTitle());
+        assertEquals(new URI(TEST_URI), item.getUri());
+        assertEquals(TEST_AUTHOR, item.getAuthor());
+        assertEquals(1, item.getPoints());
+        assertEquals(2, item.getComments());
+        assertEquals(3, item.getRank());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setAuthorThrowsIAEIfEmpty() throws URISyntaxException {
+        final NewsItem item = new NewsItem(TEST_TITLE, TEST_URI, TEST_AUTHOR);
+
+        item.setAuthor("");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void setAuthorThrowsNPEIfNull() throws URISyntaxException {
+        final NewsItem item = new NewsItem(TEST_TITLE, TEST_URI, TEST_AUTHOR);
+
+        item.setAuthor(null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setTitleThrowsIAEIfEmpty() throws URISyntaxException {
+        final NewsItem item = new NewsItem(TEST_TITLE, TEST_URI, TEST_AUTHOR);
+
+        item.setTitle("");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void setTitleThrowsNPEIfEmpty() throws URISyntaxException {
+        final NewsItem item = new NewsItem(TEST_TITLE, TEST_URI, TEST_AUTHOR);
+
+        item.setTitle(null);
+    }
+
+    @Test(expected = URISyntaxException.class)
+    public void setUriThrowsURISyntaxExceptionIfMalformed() throws URISyntaxException {
+        new NewsItem(TEST_TITLE, "https://www.bb%c.co.uk", TEST_AUTHOR);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setCommentsThrowsIAEIfNegative() throws URISyntaxException {
+        final NewsItem item = new NewsItem(TEST_TITLE, TEST_URI, TEST_AUTHOR);
+
+        item.setComments(-1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setPointsThrowsIAEIfNegative() throws URISyntaxException {
+        final NewsItem item = new NewsItem(TEST_TITLE, TEST_URI, TEST_AUTHOR);
+
+        item.setPoints(-1);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void setRankThrowsIAEIfNegative() throws URISyntaxException {
+        final NewsItem item = new NewsItem(TEST_TITLE, TEST_URI, TEST_AUTHOR);
+
+        item.setRank(-1);
+    }
+}
