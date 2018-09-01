81b3eadca3978f78fc4b593bfa18a0cc4289d286
rimadoma <richard.domander@gmail.com>
2018-09-01 17:35:12 +0100

    Add NewsItem class

---
+++ src/main/java/newsscraper/NewsItem.java
@@ -0,0 +1,79 @@
+package newsscraper;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+public class NewsItem {
+    private String title;
+    private URI uri;
+    private String author;
+    private int points;
+    private int comments;
+    private int rank;
+
+    public NewsItem(final String title, final String uri, final String author) throws URISyntaxException {
+        setTitle(title);
+        setUri(uri);
+        setAuthor(author);
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(final String author) throws IllegalArgumentException {
+        if (title.isEmpty()) {
+            throw new IllegalArgumentException("Author cannot be empty");
+        }
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(final String title) throws IllegalArgumentException {
+        if (title.isEmpty()) {
+            throw new IllegalArgumentException("Title cannot be empty");
+        }
+        this.title = title;
+    }
+
+    public URI getUri() {
+        return uri;
+    }
+
+    public void setUri(final String uri) throws URISyntaxException {
+        this.uri = new URI(uri);
+    }
+
+    public int getComments() {
+        return comments;
+    }
+
+    public void setComments(final int comments) throws IllegalArgumentException {
+        if (comments < 0) {
+            throw new IllegalArgumentException("Number of comments cannot be negative");
+        }
+        this.comments = comments;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(final int points) throws IllegalArgumentException {
+        if (points < 0) {
+            throw new IllegalArgumentException("Number of points cannot be negative");
+        }
+        this.points = points;
+    }
+
+    public int getRank() {
+        return rank;
+    }
+
+    public void setRank(int rank) throws IllegalArgumentException {
+        this.rank = rank;
+    }
+}
